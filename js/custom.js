8cbc807166c29bb09218407744a315ab8c185948
GatuZa <andrushchenko.d@ukr.net>
2014-08-01 18:23:26 +0300

    Слайдер, js-код вынесен в отдельный файл, html-валидация

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+$(document).ready(function (e){
+    var clock = $(".clock").FlipClock(10, {
+        countdown: true
+    });
+
+    // появление хедера при скролинге страницы
+    $(window).scroll(function () {
+        self = $("#header");
+        if (self.hasClass("hidden")) self.removeClass("hidden");
+        if ($(document).scrollTop() == 0) self.addClass("hidden");
+    });
+
+    // подписка на голосования (надо добавить фалидацию)
+    $("#subscribeForm").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "file.php",
+            data: $(this).serialize(),
+            success: function (response) {
+                alert("Вы подписаны. Мы будем присылать Вам уведомления о новых голосованиях.");
+            },
+            error: function (xhr, str) {
+                alert("Возникли технические неисправности. Попробуйте пожалуйста позже.");
+            }
+        });
+    });
+
+    // простой фоновый слайдер фоновых изображений
+    var images = [
+        "img/1.jpg",
+        "img/2.jpg",
+        "img/title_refer-ua.png"
+    ];
+
+    var offset = 0;
+    function slider () {
+        $(images).each(function (i, el) {
+            setTimeout(function () {
+                $(".jumbotron").css("background-image", "url(" + el + ")");
+            }, 2000 + offset);
+            offset += 2000;
+        });
+        slider();
+    };
+
+    try {
+        slider();
+    } catch (ex) {}
+});
